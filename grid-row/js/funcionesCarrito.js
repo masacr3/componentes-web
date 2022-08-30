453a37f5acefd82f71506fe3e002dd3ce7ffd4a0
masacr3(leo R.) <leorhcp2030@gmail.com>
2022-08-29 22:13:28 -0300

    refactorizo funciones de carrito migroo funcionesLI

--- grid-row/js/funcionesCarrito.js
+++ grid-row/js/funcionesCarrito.js
@@ -1,4 +1,8 @@
-function calcularTotalCarrito(){
+
+// package.action{}
+
+
+function carritoTotal(){
     var lis = document.querySelectorAll("li")
 
     var total = 0
@@ -11,7 +15,7 @@ function calcularTotalCarrito(){
 
 }
 
-function estaProducto(cod){
+function carritoEstaProducto(cod){
     var lis = document.querySelectorAll("li")
     if (lis.length === 0) return false
 
@@ -25,7 +29,7 @@ function estaProducto(cod){
     return ok
 }
 
-function actualizarProducto(cod, quitar){
+function carritoActualizarProducto(cod, quitar){
     var item = null
     document.querySelectorAll("li").forEach(it =>{
         if ( it.getAttribute("data-cod-barras") === cod) item = it
@@ -41,4 +45,95 @@ function actualizarProducto(cod, quitar){
     var cantSubtotal = item.children.item(0).children.item(2).children.item(0)
     cantSubtotal.innerHTML = parseInt(precio) * (quitar ? -1 : 1) +  parseInt(cantSubtotal.innerHTML)
     
+}
+
+function carritoAgregarProducto(cod){
+    var producto = serviceApiProducto(cod)
+
+    var ul = document.querySelector("ul")
+
+    var li = document.createElement("li")
+    li.setAttribute("data-cod-barras", producto.cod)
+    li.setAttribute("data-precio", producto.precio)
+
+    var dproducto = document.createElement("div")
+    dproducto.classList.add("producto")
+
+    var dcantidad = document.createElement("div")
+    dcantidad.classList.add("cant")
+
+    var dcantidadvalue = document.createElement("div")
+    dcantidadvalue.classList.add("value")
+    dcantidadvalue.innerHTML = producto.cantidad
+
+    var dplaceholdercant = document.createElement("div")
+    dplaceholdercant.classList.add("mute")
+    dplaceholdercant.innerHTML = "cantidad"
+
+    var darticulo =  document.createElement("div")
+    darticulo.classList.add("articulo")
+
+    var dmarca = document.createElement("div")
+    dmarca.classList.add("marca")
+    dmarca.innerHTML = producto.marca
+
+    var ddescripcion = document.createElement("div")
+    ddescripcion.classList.add("descripcion")
+    ddescripcion.classList.add("truncado-articulos")
+    ddescripcion.innerHTML = producto.descripcion 
+
+
+    var dsubtotal = document.createElement("div")
+    dsubtotal.classList.add("subtotal")
+
+    var dsubtotalvalue = document.createElement("div")
+    dsubtotalvalue.classList.add("value")
+    dsubtotalvalue.innerHTML = producto.precio
+
+    var dplaceholdersubtotal = document.createElement("div")
+    dplaceholdersubtotal.classList.add("mute-der")
+    dplaceholdersubtotal.innerHTML = "subtotal"
+
+    var dborrar = document.createElement("div")
+    dborrar.classList.add("borrar")
+    dborrar.classList.add("no-show")
+
+    var dtitulo = document.createElement("div")
+    dtitulo.classList.add("titulo")
+    dtitulo.innerHTML = "Borrar articulo?"
+
+    var dok = document.createElement("div")
+    dok.classList.add("ok")
+    dok.innerHTML = "Ok"
+
+    var dcancel = document.createElement("div")
+    dcancel.classList.add("cancel")
+    dcancel.innerHTML = "Cancelar"
+
+    //armamos el arbol de dependencias
+
+    dcantidad.appendChild(dcantidadvalue)
+    dcantidad.appendChild(dplaceholdercant)
+    darticulo.appendChild(dmarca)
+    darticulo.appendChild(ddescripcion)
+    dsubtotal.appendChild(dsubtotalvalue)
+    dsubtotal.appendChild(dplaceholdersubtotal)
+    dproducto.appendChild(dcantidad)
+    dproducto.appendChild(darticulo)
+    dproducto.appendChild(dsubtotal)
+    dborrar.appendChild(dtitulo)
+    dborrar.appendChild(dok)
+    dborrar.appendChild(dcancel)
+
+    li.appendChild(dproducto)
+    li.appendChild(dborrar)
+
+    ul.appendChild(li)
+
+
+    //agrego el scrolling si hay mas de X li en el UL
+    if ( document.querySelectorAll("li").length > 5 ){
+        ul.classList.add("scrolling")
+    }
+  
 }
--- grid-row/js/funcionesLi.js
+++ grid-row/js/funcionesLi.js
@@ -13,76 +13,76 @@ function agregarli(cod, marca, descripcion, cant, precio){
         ul.removeChild(document.querySelectorAll("li")[1])
     }
 
-    var li = document.createElement("LI")
+    // var li = document.createElement("LI")
 
-    li.setAttribute("data-cod-barras", cod)
-    li.setAttribute("data-precio",precio)
+    // li.setAttribute("data-cod-barras", cod)
+    // li.setAttribute("data-precio",precio)
 
-    var _producto = document.createElement("DIV")
-    var _cant = document.createElement("DIV")
-    var _cantValue = document.createElement("DIV")
-    var placeholderCant = document.createElement("DIV")
-    var _articulo = document.createElement("DIV")
-    var _marca = document.createElement("DIV")
-    var _descripcion = document.createElement("DIV")
-    var _subtotal = document.createElement("DIV")
-    var _cantSubtotal = document.createElement("DIV")
-    var placeholderSubtotal = document.createElement("DIV")
-    var borrar = document.createElement("DIV")
-    var titulo = document.createElement("DIV")
-    var ok = document.createElement("DIV")
-    var cancel = document.createElement("DIV")
+    // var _producto = document.createElement("DIV")
+    // var _cant = document.createElement("DIV")
+    // var _cantValue = document.createElement("DIV")
+    // var placeholderCant = document.createElement("DIV")
+    // var _articulo = document.createElement("DIV")
+    // var _marca = document.createElement("DIV")
+    // var _descripcion = document.createElement("DIV")
+    // var _subtotal = document.createElement("DIV")
+    // var _cantSubtotal = document.createElement("DIV")
+    // var placeholderSubtotal = document.createElement("DIV")
+    // var borrar = document.createElement("DIV")
+    // var titulo = document.createElement("DIV")
+    // var ok = document.createElement("DIV")
+    // var cancel = document.createElement("DIV")
 
     //agregamos las clases correspondientes
-    _producto.classList.add("producto")
-    _cant.classList.add("cant")
-    _cantValue.classList.add("value")
-    placeholderCant.classList.add("mute")
-    _articulo.classList.add("articulo")
-    _marca.classList.add("marca")
-    _descripcion.classList.add("descripcion")
-    _descripcion.classList.add("truncado-articulos")
-    _subtotal.classList.add("subtotal")
-    _cantSubtotal.classList.add("value")
-    placeholderSubtotal.classList.add("mute-der")
-    borrar.classList.add("borrar")
-    borrar.classList.add("no-show")
-    titulo.classList.add("titulo")
-    ok.classList.add("ok")
-    cancel.classList.add("cancel")
+    // _producto.classList.add("producto")
+    // _cant.classList.add("cant")
+    // _cantValue.classList.add("value")
+    // placeholderCant.classList.add("mute")
+    // _articulo.classList.add("articulo")
+    // _marca.classList.add("marca")
+    // _descripcion.classList.add("descripcion")
+    // _descripcion.classList.add("truncado-articulos")
+    // _subtotal.classList.add("subtotal")
+    // _cantSubtotal.classList.add("value")
+    // placeholderSubtotal.classList.add("mute-der")
+    // borrar.classList.add("borrar")
+    // borrar.classList.add("no-show")
+    // titulo.classList.add("titulo")
+    // ok.classList.add("ok")
+    // cancel.classList.add("cancel")
     
     //agregamos el texto correspondiente
-    _cantValue.innerHTML = cant
-    placeholderCant.innerHTML = "cantidad"
-    _marca.innerHTML = marca
-    _descripcion.innerHTML = descripcion
-    _cantSubtotal.innerHTML = precio
-    placeholderSubtotal.innerHTML = "subtotal"
-    titulo.innerHTML = "Borrar articulo?"
-    ok.innerHTML = "Ok"
-    cancel.innerHTML = "Cancelar"
+    // _cantValue.innerHTML = cant
+    // placeholderCant.innerHTML = "cantidad"
+    // _marca.innerHTML = marca
+    // _descripcion.innerHTML = descripcion
+    // _cantSubtotal.innerHTML = precio
+    // placeholderSubtotal.innerHTML = "subtotal"
+    // titulo.innerHTML = "Borrar articulo?"
+    // ok.innerHTML = "Ok"
+    // cancel.innerHTML = "Cancelar"
 
 
     //agregamos dependencia entre objetos
-    _cant.append(_cantValue)
-    _cant.appendChild(placeholderCant)
-    _articulo.appendChild(_marca)
-    _articulo.appendChild(_descripcion)
-    _subtotal.appendChild(_cantSubtotal)
-    _subtotal.appendChild(placeholderSubtotal)
-    _producto.appendChild(_cant)
-    _producto.appendChild(_articulo)
-    _producto.appendChild(_subtotal)
-    borrar.appendChild(titulo)
-    borrar.appendChild(ok)
-    borrar.appendChild(cancel)
+    // _cant.append(_cantValue)
+    // _cant.appendChild(placeholderCant)
+    // _articulo.appendChild(_marca)
+    // _articulo.appendChild(_descripcion)
+    // _subtotal.appendChild(_cantSubtotal)
+    // _subtotal.appendChild(placeholderSubtotal)
+    // _producto.appendChild(_cant)
+    // _producto.appendChild(_articulo)
+    // _producto.appendChild(_subtotal)
+    // borrar.appendChild(titulo)
+    // borrar.appendChild(ok)
+    // borrar.appendChild(cancel)
 
-    li.appendChild(_producto)
-    li.appendChild(borrar)
+    // li.appendChild(_producto)
+    // li.appendChild(borrar)
 
-    ul.appendChild(li)
+    // ul.appendChild(li)
 
-    calcularTotalCarrito()
+    // calcularTotalCarrito()
 
     if ( document.querySelectorAll("li").length > 5 ){
         ul.classList.add("scrolling")
